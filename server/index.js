4ecbf3573f32de7e0fdb21e45590923b48f64ce9
Prakash Singh <prakashsingh1145786@gmail.com>
2026-08-13 09:19:06 +0530

    Fix production CORS

--- server/index.js
+++ server/index.js
@@ -25,12 +25,14 @@ app.post(
   stripeWebhook
 );
 
-app.use(cors(
-    {origin:"http://localhost:5173",
-        credentials:true,
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-    }
-))
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://examnotesai-1-pm9w.onrender.com"
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+}))
 
 
 
